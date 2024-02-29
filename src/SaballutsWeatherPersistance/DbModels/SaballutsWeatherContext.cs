37acf647ad5e75359d1fbc9df6f59fba3d210291
disasterdev500 <155431384+disasterdev500@users.noreply.github.com>
2024-02-29 12:50:01 +0100

    persistance: add saballut's weather context (#9)

---
+++ src/SaballutsWeatherPersistance/DbModels/SaballutsWeatherContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SaballutsWeatherPersistance.DbModels;
+
+public class SaballutsWeatherContext(DbContextOptions<SaballutsWeatherContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        _ = modelBuilder.Entity<DbWeatherRecord>()
+            .Property(e => e.Date)
+            .HasConversion(new UtcDateTimeConverter());
+        _ = modelBuilder.Entity<DbDailyWeatherStats>()
+            .Property(e => e.Date)
+            .HasConversion(new UtcDateTimeConverter());
+    }
+
+    public DbSet<DbWeatherRecord> WeatherRecords { get; set; }
+    public DbSet<DbDailyWeatherStats> DailyWeatherStats { get; set; }
+}
---
+++ src/SaballutsWeatherPersistance/DbModels/UtcDateTimeConverter.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace SaballutsWeatherPersistance.DbModels;
+
+public class UtcDateTimeConverter : ValueConverter<DateTime, DateTime>
+{
+    public UtcDateTimeConverter(ConverterMappingHints? mappingHints = null)
+        : base(
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc),   // Indicate that DateTime is in UTC
+            v => v,   // Convert UTC DateTime back to DateTime without modification
+            mappingHints)
+    { }
+}
