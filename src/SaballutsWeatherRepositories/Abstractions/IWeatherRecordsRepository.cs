b5bfb23788d608f2b4dfac94b82fb0a7cdddf55b
disaster <disasterdev404@gmail.com>
2024-03-02 18:48:13 +0100

    repositories: add functionality to weather record repository

--- src/SaballutsWeatherRepositories/Abstractions/IWeatherRecordsRepository.cs
+++
@@ -1,8 +0,0 @@
-using SaballutsWeatherDomain.Models;
-
-namespace SaballutsWeatherRepositories.Abstractions;
-
-public interface IWeatherRecordsRepository
-{
-    Task<WeatherRecord> GetById(DateTime id);
-}
