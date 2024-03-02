a9675148052f70c5e16fdb1e0e9bf4e9e0e80559
disaster <disasterdev404@gmail.com>
2024-03-02 23:34:21 +0100

    application: add functionality to weather record service

--- src/SaballutsWeatherApplication/Behaviors/WeatherRecord/WeatherRecordService.cs
+++ src/SaballutsWeatherApplication/Behaviors/WeatherRecord/WeatherRecordService.cs
@@ -13,7 +13,20 @@ public class WeatherRecordService(IWeatherRecordsRepository weatherRecordReposit
 
     public List<WeatherRecord> GetByDateRange(DateTime initial, DateTime final) => _weatherRecordRepository.GetByIntervalTime(initial, final);
 
-    public async Task AddAsync(WeatherRecord weatherRecord) => await _weatherRecordRepository.AddAsync(weatherRecord);
+    public async Task AddAsync(WeatherRecord weatherRecord)
+    {
+        var record = await _weatherRecordRepository.GetById(weatherRecord.Date);
+        if (record is not null)
+        {
+            return;
+        }
+        await _weatherRecordRepository.AddAsync(weatherRecord);
+        await _weatherRecordRepository.SaveAsync();
+    }
 
-    public async Task AddRangeAsync(List<WeatherRecord> weatherRecords) => await _weatherRecordRepository.AddRangeAsync(weatherRecords);
+    public async Task AddRangeAsync(List<WeatherRecord> weatherRecords)
+    {
+        await _weatherRecordRepository.AddRangeAsync(weatherRecords);
+        await _weatherRecordRepository.SaveAsync();
+    }
 }
