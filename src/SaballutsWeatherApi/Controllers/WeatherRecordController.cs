25bfe24d7d07f3a08d0c2d9596442c00172bb722
disasterdev500 <155431384+disasterdev500@users.noreply.github.com>
2024-03-02 23:36:36 +0100

    api: add new endpoint to weather record controller (#29)

--- src/SaballutsWeatherApi/Controllers/WeatherRecordController.cs
+++ src/SaballutsWeatherApi/Controllers/WeatherRecordController.cs
@@ -14,7 +14,6 @@ public class WeatherRecordController(IWeatherRecordService weatherRecordGetterSe
     [HttpGet("{timestamp}")]
     public async Task<ActionResult<WeatherRecord>> GetWeatherRecordAsync(DateTime timestamp)
     {
-        System.Console.WriteLine(timestamp);
         WeatherRecord? record = null;
         try
         {
@@ -22,9 +21,55 @@ public class WeatherRecordController(IWeatherRecordService weatherRecordGetterSe
         }
         catch (Exception e)
         {
-            System.Console.WriteLine($"Error en GetWeatherRecordAsync - Exception: {e}");
+            return BadRequest(e);
         }
 
         return (record == null) ? NotFound() : Ok(record);
+    }
+
+    [HttpGet()]
+    public async Task<ActionResult<List<WeatherRecord>>> GetWeatherRecordByDateRange([FromQuery] DateTime initial, [FromQuery] DateTime final)
+    {
+        List<WeatherRecord>? records = null;
+        try
+        {
+            records = _weatherRecordGetterService.GetByDateRange(initial, final);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+
+        return (records == null) ? NotFound() : Ok(records);
+    }
+
+    [HttpPost()]
+    public async Task<ActionResult> CreateWeatherRecordAsync(WeatherRecord weatherRecord)
+    {
+        try
+        {
+            await _weatherRecordGetterService.AddAsync(weatherRecord);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+
+        return NoContent();
+    }
+
+    [HttpPost("load")]
+    public async Task<ActionResult> CreateBatchWeatherRecordAsync(List<WeatherRecord> weatherRecords)
+    {
+        try
+        {
+            await _weatherRecordGetterService.AddRangeAsync(weatherRecords);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+
+        return NoContent();
     }
 }
--- src/SaballutsWeatherApplication/Abstractions/IWeatherRecordService.cs
+++ src/SaballutsWeatherApplication/Abstractions/IWeatherRecordService.cs
@@ -5,4 +5,7 @@ namespace SaballutsWeatherApplication.Abstractions;
 public interface IWeatherRecordService
 {
     Task<WeatherRecord> GetByIDAsync(DateTime timestamp);
+    List<WeatherRecord> GetByDateRange(DateTime initial, DateTime final);
+    Task AddAsync(WeatherRecord weatherRecord);
+    Task AddRangeAsync(List<WeatherRecord> weatherRecords);
 }
