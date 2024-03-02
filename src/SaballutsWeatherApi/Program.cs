176465c2d0c708ce1eabe1e7839d05d0369a3688
disaster <disasterdev404@gmail.com>
2024-03-02 13:12:07 +0100

    api: add dependencies

--- src/SaballutsWeatherApi/Program.cs
+++ src/SaballutsWeatherApi/Program.cs
@@ -1,19 +1,41 @@
-var builder = WebApplication.CreateBuilder(args);
+using SaballutsWeatherApplication;
+using SaballutsWeatherUtilities;
+using SaballutsWeatherRepositories;
+using SaballutsWeatherPersistance;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        // Saballut's Weather services
+        builder.Services.AddApplication();
+        builder.Services.AddUtilities();
+        builder.Services.AddRepositories();
+        builder.Services.AddPersistance(builder.Configuration);
+
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.Run();
