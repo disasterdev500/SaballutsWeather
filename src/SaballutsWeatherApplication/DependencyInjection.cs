c166dd7dd1473915ebabba4930d67d5d47d2f76c
disasterdev500 <155431384+disasterdev500@users.noreply.github.com>
2024-03-03 11:28:11 +0100

    general: add new services and new repositories to dependency injection container (#43)

--- src/SaballutsWeatherApplication/DependencyInjection.cs
+++ src/SaballutsWeatherApplication/DependencyInjection.cs
@@ -9,6 +9,10 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IWeatherRecordService, WeatherRecordService>();
+        services.AddScoped<IDailyWeatherStatsService, DailyWeatherStatsService>();
+        services.AddScoped<IWeeklyWeatherStatsService, WeeklyWeatherStatsService>();
+        services.AddScoped<IMonthlyWeatherStatsService, MonthlyWeatherStatsService>();
+        services.AddScoped<IYearlyWeatherStatsService, YearlyWeatherStatsService>();
 
         return services;
     }
--- src/SaballutsWeatherRepositories/DependencyInjection.cs
+++ src/SaballutsWeatherRepositories/DependencyInjection.cs
@@ -10,6 +10,10 @@ public static class DependencyInjection
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddScoped<IWeatherRecordsRepository, WeatherRecordsRepository>();
+        services.AddScoped<IDailyWeatherStatsRepository, DailyWeatherStatsRepository>();
+        services.AddScoped<IWeeklyWeatherStatsRepository, WeeklyWeatherStatsRepository>();
+        services.AddScoped<IMonthlyWeatherStatsRepository, MonthlyWeatherStatsRepository>();
+        services.AddScoped<IYearlyWeatherStatsRepository, YearlyWeatherStatsRepository>();
 
         return services;
     }
