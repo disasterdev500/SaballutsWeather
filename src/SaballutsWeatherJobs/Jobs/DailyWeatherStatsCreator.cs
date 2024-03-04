3a831ebc9b0560a3f54619665e8fff785de7ba50
disasterdev500 <155431384+disasterdev500@users.noreply.github.com>
2024-03-04 23:41:16 +0100

    jobs: add daily weather stats creator job (#59)

--- src/SaballutsWeatherJobs/Jobs/DailyWeatherStatsCreator.cs
+++ src/SaballutsWeatherJobs/Jobs/DailyWeatherStatsCreator.cs
@@ -1,12 +1,26 @@
 using Quartz;
+using SaballutsWeatherApplication.Behaviors;
 
 namespace SaballutsWeatherJobs.Jobs;
 
 [DisallowConcurrentExecution]
-public class DailyWeatherStatsCreator() : IJob
+public class DailyWeatherStatsCreator(DailyWeatherStatsService dailyWeatherStatsService) : IJob
 {
+    private readonly DailyWeatherStatsService _dailyWeatherStatsService = dailyWeatherStatsService;
     public async Task Execute(IJobExecutionContext context)
     {
-        System.Console.WriteLine($"{DateTime.UtcNow} Execute");
+        try
+        {
+            var createResult = await _dailyWeatherStatsService.GenerateDailyWeatherStatsSinceLastAsync();
+            if (createResult.IsFailure)
+            {
+                System.Console.WriteLine($"{DateTime.UtcNow}: Error in Job {context.JobDetail.JobType}. Error: {createResult.Error}");
+            }
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine($"{DateTime.UtcNow}: Error in Job {context.JobDetail.JobType}. Error: Unexpected error. Exception: {e}");
+        }
+        System.Console.WriteLine($"{DateTime.UtcNow}: Job {context.JobDetail.JobType} finished successfully");
     }
 }
