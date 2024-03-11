7078dce9d5c267d6b81ea3011b3ab43fe863ca07
disasterdev500 <155431384+disasterdev500@users.noreply.github.com>
2024-03-12 00:24:22 +0100

    utilities: improve mappers (#74)

--- src/SaballutsWeatherUtilities/Mappers/MappingProfile.cs
+++ src/SaballutsWeatherUtilities/Mappers/MappingProfile.cs
@@ -11,16 +11,24 @@ public class MappingProfile : Profile
         CreateMap<WeatherRecord, DbWeatherRecord>();
         CreateMap<DbWeatherRecord, WeatherRecord>();
 
-        CreateMap<DailyWeatherStats, DbDailyWeatherStats>();
-        CreateMap<DbDailyWeatherStats, DailyWeatherStats>();
+        CreateMap<DailyWeatherStats, DbDailyWeatherStats>().
+            ForMember(db => db.Date, m => m.MapFrom(m => m.Id));
+        CreateMap<DbDailyWeatherStats, DailyWeatherStats>().
+            ForMember(db => db.Id, m => m.MapFrom(m => m.Date));
 
-        CreateMap<WeeklyWeatherStats, DbWeeklyWeatherStats>();
-        CreateMap<DbWeeklyWeatherStats, WeeklyWeatherStats>();
+        CreateMap<WeeklyWeatherStats, DbWeeklyWeatherStats>().
+            ForMember(db => db.Date, m => m.MapFrom(m => m.Id));
+        CreateMap<DbWeeklyWeatherStats, WeeklyWeatherStats>().
+            ForMember(db => db.Id, m => m.MapFrom(m => m.Date));
 
-        CreateMap<MonthlyWeatherStats, DbMonthlyWeatherStats>();
-        CreateMap<DbMonthlyWeatherStats, MonthlyWeatherStats>();
+        CreateMap<MonthlyWeatherStats, DbMonthlyWeatherStats>().
+            ForMember(db => db.Date, m => m.MapFrom(m => m.Id));
+        CreateMap<DbMonthlyWeatherStats, MonthlyWeatherStats>().
+            ForMember(db => db.Id, m => m.MapFrom(m => m.Date));
 
-        CreateMap<YearlyWeatherStats, DbYearlyWeatherStats>();
-        CreateMap<DbYearlyWeatherStats, YearlyWeatherStats>();
+        CreateMap<YearlyWeatherStats, DbYearlyWeatherStats>().
+            ForMember(db => db.Date, m => m.MapFrom(m => m.Id));
+        CreateMap<DbYearlyWeatherStats, YearlyWeatherStats>().
+            ForMember(db => db.Id, m => m.MapFrom(m => m.Date));
     }
 }
