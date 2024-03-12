1d23568c039ae54c606054fbbeecdc59896bcc1c
disaster <disasterdev404@gmail.com>
2024-03-12 11:58:46 +0100

    application: add abstractions for infrastructure and repository

---
+++ src/SaballutsWeatherApplication/Common/Abstractions/Repositories/IWeeklyWeatherStatsRepository.cs
@@ -0,0 +1,14 @@
+using SaballutsWeatherDomain.Models;
+
+namespace SaballutsWeatherApplication.Common.Abstractions.Repositories;
+
+public interface IWeeklyWeatherStatsRepository
+{
+    Task<WeeklyWeatherStats> GetById(DateTime id);
+    Task<List<WeeklyWeatherStats>> GetByIntervalTime(DateTime initial, DateTime final);
+    Task<WeeklyWeatherStats> GetFirstAsync();
+    Task<WeeklyWeatherStats> GetLastAsync();
+    Task AddAsync(WeeklyWeatherStats weeklyWeatherStats);
+    Task AddRangeAsync(List<WeeklyWeatherStats> weeklyWeatherStats);
+    Task SaveAsync();
+}
---
+++ src/SaballutsWeatherApplication/Common/Abstractions/Repositories/IYearlyWeatherStatsRepository.cs
@@ -0,0 +1,13 @@
+using SaballutsWeatherDomain.Models;
+
+namespace SaballutsWeatherApplication.Common.Abstractions.Repositories;
+
+public interface IYearlyWeatherStatsRepository
+{
+    Task<YearlyWeatherStats> GetById(DateTime id);
+    Task<List<YearlyWeatherStats>> GetByIntervalTimeAsync(DateTime initial, DateTime final);
+    Task<YearlyWeatherStats> GetLastAsync();
+    Task AddAsync(YearlyWeatherStats yearlyWeatherStats);
+    Task AddRangeAsync(List<YearlyWeatherStats> yearlyWeatherStats);
+    Task SaveAsync();
+}
