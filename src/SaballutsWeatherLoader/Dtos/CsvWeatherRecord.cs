913d6096f3a1d3d43101ec6ec5d2d0329a56b5ec
disaster <disasterdev404@gmail.com>
2024-03-12 12:04:47 +0100

    loader: add csv weather record dto

---
+++ src/SaballutsWeatherLoader/Dtos/CsvWeatherRecord.cs
@@ -0,0 +1,50 @@
+
+namespace SaballutsWeatherLoader.Dtos;
+
+public class CsvWeatherRecord
+{
+    public string Timestamp { get; set; }
+    public double IndoorTemperature { get; set; }
+    public double IndoorHumidity { get; set; }
+    public double OutdoorTemperature { get; set; }
+    public double OutdoorHumidity { get; set; }
+    public double DewPoint { get; set; }
+    public double ThermalSensation { get; set; }
+    public double WindSpeed { get; set; }
+    public double GustSpeed { get; set; }
+    public double WindDirection { get; set; }
+    public double AbsolutePressure { get; set; }
+    public double RelativePressure { get; set; }
+    public double SolarRadiation { get; set; }
+    public double UVI { get; set; }
+    public double RainPerHour { get; set; }
+    public double RainEpisode { get; set; }
+    public double RainPerDay { get; set; }
+    public double RainPerWeek { get; set; }
+    public double RainPerMonth { get; set; }
+    public double RainPerYear { get; set; }
+
+    public override string ToString()
+    {
+        return $"Timestamp: {Timestamp}, " +
+            $"IndoorTemperature: {IndoorTemperature}°C(degree Celsius), " +
+            $"IndoorHumidity: {IndoorHumidity}%(percentage), " +
+            $"OutdoorTemperature: {OutdoorTemperature}°C(degree Celsius), " +
+            $"OutdoorHumidity: {OutdoorHumidity}%(percentage), " +
+            $"DewPoint: {DewPoint}°C(degree Celsius), " +
+            $"ThermalSensation: {ThermalSensation}°C(degree Celsius), " +
+            $"WindSpeed: {WindSpeed} km/h (Kilometres per hour), " +
+            $"GustSpeed: {GustSpeed} km/h (Kilometres per hour), " +
+            $"WindDirection: {WindDirection}°, " +
+            $"AbsolutePressure: {AbsolutePressure} hPa (Hectopascal), " +
+            $"RelativePressure: {RelativePressure} hPa (Hectopascal), " +
+            $"SolarRadiation: {SolarRadiation} W/m²(watts per square metre), " +
+            $"UVI: {UVI} (Ultra Violet Radiation Index ), " +
+            $"RainPerHour: {RainPerHour} mm(milliliter), " +
+            $"RainEpisode: {RainEpisode} mm(milliliter), " +
+            $"RainPerDay: {RainPerDay} mm(milliliter), " +
+            $"RainPerWeek: {RainPerWeek} mm(milliliter), " +
+            $"RainPerMonth: {RainPerMonth} mm(milliliter), " +
+            $"RainPerYear: {RainPerYear} mm(milliliter)";
+    }
+}
